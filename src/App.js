1e157a70b8f9b420cf1a7c988f4b90c4a184a2b0
edwardspresume <edwardspresume@aol.com>
2021-03-06 19:30:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import AppMenu from './components/AppMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <AppMenu />
+
+      <main id="main-content">
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TaskTable.js
@@ -0,0 +1,9 @@
+function TaskTable() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default TaskTable
